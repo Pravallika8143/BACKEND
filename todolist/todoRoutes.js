3b38c689f7987c96d1f38dbf25a7882b3323c500
Pravallika8143 <mamidisettipravallika4@gmail.com>
2026-03-26 09:02:02 +0530

    routes

--- todolist/todoRoutes.js
+++ todolist/todoRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const todoController = require("../todolist/todoController");
 
 router.get("/", todoController.getTodos);
-// router.get("/:id", todoController.getTodoById);
 router.post("/", todoController.addTodo);
 router.put("/:id", todoController.updateTodo);
 router.delete("/:id", todoController.deleteTodo);
